1a9ce618abcee133bd3a4ff093448ae4d5f97ce0
packagefather <oicinvestments@gmail.com>
2024-01-05 19:05:51 +0100

    Initial Commit

--- cosmwasm/src/bin/schema.rs
+++ cosmwasm/src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use cosmwasm::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cosm_wasm::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
--- cosmwasm/src/contract.rs
+++ cosmwasm/src/contract.rs
@@ -8,7 +8,7 @@ use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 /*
 // version info for migration info
-const CONTRACT_NAME: &str = "crates.io:cosmwasm";
+const CONTRACT_NAME: &str = "crates.io:cosm-wasm";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 */
 
